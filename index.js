7b242e67a29da46e8d9b24e2933d23fd17998260
mohamed <elmourabitmohamed05@gmail.com>
2023-11-21 19:55:32 +0100

    we commited three modifications

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var h1 = document.getElementById('h1');
 h1.addEventListener('click', ()=>{
-    h1.style.fontSize = 40;
     h1.style.color = '#ff5959';
 })
+var p = document.getElementById('p')
+p.onmousemove = function fun(){
+    p.style.fontSize = '45px';
+    p.style.color = '#ff5959';
+}
+p.onmouseout = function fun2(){
+    p.style.color = 'black';
+}
